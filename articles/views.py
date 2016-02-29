3532c191c047ad490665da586678550791f18668
Duy Bao Nguyen <bduyng@gmail.com>
2016-02-29 17:40:38 +0700

    add articles index page and apply the view

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Article
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the articles index.")
+    latest_article_list = Article.objects.order_by('-created_time')[:5]
+    context = {'latest_article_list': latest_article_list}
+    return render(request, 'articles/index.html', context)
