fc8e2f6142136cce153ed210920d484937483b06
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-01 10:44:55 +0700

    include built-in auth.urls

--- jsnews/urls.py
+++ jsnews/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    # https://docs.djangoproject.com/en/1.9/topics/auth/default/#using-the-views
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^', include('articles.urls')),
     url(r'^admin/', admin.site.urls),
 ]
