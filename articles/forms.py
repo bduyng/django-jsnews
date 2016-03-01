480bfd515700ae8efd8a603a380b8943afe34698
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-01 16:57:24 +0700

    incomplete registration process

--- articles/forms.py
+++ articles/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
 
 
@@ -24,3 +24,36 @@ class LoginForm(AuthenticationForm):
             'type': 'password'
         })
     )
+
+
+class RegisterForm(UserCreationForm):
+        username = forms.CharField(
+            label='Email',
+            max_length=254,
+            widget=forms.TextInput(attrs={
+                'class': 'form-control',
+                'name': 'username',
+                'placeholder': 'Email',
+                'type': 'text'
+            })
+        )
+        password1 = forms.CharField(
+            label='Password',
+            max_length=254,
+            widget=forms.TextInput(attrs={
+                'class': 'form-control',
+                'name': 'password',
+                'placeholder': 'Password',
+                'type': 'password'
+            })
+        )
+        password2 = forms.CharField(
+            label='Password Confirmation',
+            max_length=254,
+            widget=forms.TextInput(attrs={
+                'class': 'form-control',
+                'name': 'password',
+                'placeholder': 'Password Confirmation',
+                'type': 'password'
+            })
+        )
--- jsnews/urls.py
+++ jsnews/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from articles.forms import LoginForm
+from articles.forms import LoginForm, RegisterForm
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -9,4 +10,10 @@ urlpatterns = [
     # https://docs.djangoproject.com/en/1.9/topics/auth/default/#all-authentication-views
     url(r'^login/$', auth_views.login, {'authentication_form': LoginForm}),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}),
+
+    url(r'^register/$', CreateView.as_view(
+        template_name='registration/register.html',
+        form_class=RegisterForm,
+        success_url='/'
+    )),
 ]
