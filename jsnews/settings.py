502b08899cd2235256e4b9a3493a5c3c609bc0b9
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-01 10:46:53 +0700

    define LOGIN_REDIRECT_URL

--- jsnews/settings.py
+++ jsnews/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Login Redirect URL
+# https://docs.djangoproject.com/en/1.9/ref/settings/#login-redirect-url
+
+LOGIN_REDIRECT_URL = '/'
