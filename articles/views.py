08814c4824355e047fb8d8b71eca667ae373729b
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-01 09:07:31 +0700

    add pagination for article list

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,19 @@
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Article
 
 
 def index(request):
-    latest_article_list = Article.objects.order_by('-created_time')[:5]
-    context = {'latest_article_list': latest_article_list}
-    return render(request, 'articles/index.html', context)
+    latest_article_list = Article.objects.order_by('-created_time')
+    paginator = Paginator(latest_article_list, 5)
+    page = request.GET.get('page')
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        articles = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        articles = paginator.page(paginator.num_pages)
+
+    return render(request, 'articles/index.html', {'articles': articles})
