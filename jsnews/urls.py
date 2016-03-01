f7e05668f5c1d72714b2f7c871f27f34f25833da
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-01 11:27:30 +0700

    update the login url to use custom form

--- jsnews/urls.py
+++ jsnews/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from articles.forms import LoginForm
 
 urlpatterns = [
-    # https://docs.djangoproject.com/en/1.9/topics/auth/default/#using-the-views
-    url(r'^', include('django.contrib.auth.urls')),
-    url(r'^', include('articles.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('articles.urls')),
+    # https://docs.djangoproject.com/en/1.9/topics/auth/default/#all-authentication-views
+    url(r'^login/$', auth_views.login, {'authentication_form': LoginForm}),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}),
 ]
